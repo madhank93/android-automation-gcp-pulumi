b48d22c6b2acbb920822e97de815d8901a528660
madhank93 <madhankumaravelu93@gmail.com>
2022-01-22 22:45:03 +0530

    chore: removing command.ts file and adding condition in shell script

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo 'Waiting for cloud-init...'; sleep 1; done
+
 
 sudo apt-get update
 
 sudo apt-get -y install docker.io docker-compose
 
 # add current user to docker group so there is no need to use sudo when running docker
-sudo usermod -aG docker $(whoami)
+# sudo usermod -aG docker $(whoami)
