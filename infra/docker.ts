d7123035295fd483d863df86d06ef07d98d987f9
madhank93 <madhankumaravelu93@gmail.com>
2022-04-10 19:32:54 +0530

    feat: added network, selenoid and selenoid ui container

--- infra/docker.ts
+++ infra/docker.ts
@@ -16,27 +16,21 @@ const remoteInstance = new docker.Provider(
   { dependsOn: dockerInstallation }
 );
 
+const network = new docker.Network(
+  "selenoid",
+  { name: "selenoid" },
+  { provider: remoteInstance }
+);
+
 const androidImage = new docker.RemoteImage(
   "android-image",
   {
-    name: "selenoid/android:10.0",
+    name: "selenoid/android:6.0",
   },
   {
     provider: remoteInstance,
   }
 );
-
-// const androidContainer = new docker.Container(
-//   "android-container",
-//   {
-//     image: androidImage.name,
-//     command: ["--privileged"],
-//     restart: "on-failure",
-//   },
-//   {
-//     provider: remoteInstance,
-//   }
-// );
 
 const selenoidImage = new docker.RemoteImage(
   "selenoid-image",
@@ -49,17 +43,66 @@ const selenoidImage = new docker.RemoteImage(
   }
 );
 
+const selenoidUiImage = new docker.RemoteImage(
+  "selenoid-ui-image",
+  {
+    name: "aerokube/selenoid-ui:latest",
+  },
+  {
+    provider: remoteInstance,
+    dependsOn: selenoidImage,
+  }
+);
+
 const selenoidContainer = new docker.Container(
   "selenoid-container",
   {
     image: selenoidImage.name,
     ports: [{ internal: 4444, external: 4444 }],
     name: "selenoid",
+    networksAdvanced: [{ name: network.name }],
+    volumes: [
+      {
+        hostPath: "/var/run/docker.sock",
+        containerPath: "/var/run/docker.sock",
+      },
+      {
+        hostPath: "/home/madhankumaravelu93",
+        containerPath: "/etc/selenoid",
+      },
+    ],
+    command: [
+      "-conf",
+      "/etc/selenoid/browsers.json",
+      "-container-network",
+      "selenoid",
+      "-log-output-dir",
+      "/opt/selenoid/logs",
+      "-limit",
+      "5",
+      "-save-all-logs",
+      "-service-startup-timeout",
+      "5m",
+      "-session-attempt-timeout",
+      "5m",
+    ],
   },
   {
     provider: remoteInstance,
     dependsOn: selenoidImage,
   }
+);
+
+const selenoidUiContainer = new docker.Container(
+  "selenoid-ui-container",
+  {
+    image: selenoidUiImage.name,
+    name: "selenoid-ui",
+    networksAdvanced: [{ name: network.name }],
+    ports: [{ internal: 8080, external: 8080 }],
+    command: ["--selenoid-uri", "http://selenoid:4444"],
+  },
+  { provider: remoteInstance, dependsOn: selenoidContainer }
 );
 
 export const selenoid = selenoidContainer;
